f15414b3bd1b0170e464d2a165be72aa55e89969
Anik Sen <75422517+senanik360@users.noreply.github.com>
2022-02-02 09:13:20 +0600

    Add files via upload

---
+++ Task1/Task1.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <title>Registration form</title>
+    
+</head>  
+<body>
+    <h1>Registration Form </h1>
+<form> 
+    <table>
+        <tr>
+            <td>
+               Fisrt Name : 
+                <td>
+                    <td>
+                        <input type="text" name="First Name">
+</td>
+<tr>
+            <td>
+               Last Name : 
+                <td>
+                    <td>
+                        <input type="text" name="Last Name">
+</td>
+<tr>
+            <td>
+               Age : 
+                <td>
+                    <td>
+                        <input type="text" name="Age">
+</td>
+</tr>
+<tr>
+            <td>
+               Designation : 
+                <td>
+                    <td>
+                    <input type="radio" name="Desgination">Junior Programmer
+                    <input type="radio" name="Desgination">Senior Programmer
+                    <input type="radio" name="Desgination">Project Lead
+</td>
+</tr>
+<tr>
+            <td>
+               Preferred Language : 
+                <td>
+                    <td>
+                    <input type="checkbox" name="Preferred Language">JAVA
+                    <input type="checkbox" name="Preferred Language"> PHP
+                    <input type="checkbox" name="Preferred Language"> C++
+</td>
+</tr>
+<tr>
+            <td>
+               Email : 
+                <td>
+                    <td>
+                    <input type="email" name="Email">
+</td>
+</tr>
+<tr>
+            <td>
+               Password : 
+                <td>
+                    <td>
+                    <input type="password" name="Password">
+</td>
+</tr>
+<tr>
+            <td>
+               Please choose a file  
+                <td>
+                    <td>
+                    <input type="file">
+</td>
+</tr>
+<tr>
+                    <td>
+                    <input type="submit">
+                    <input type="reset">
+
+</td>
+</tr>
+</body> 
+</html>
