287125ffc085b8eeeba577e89a8932f02b6eae1e
Anik Sen <75422517+senanik360@users.noreply.github.com>
2022-02-16 21:29:20 +0600

    Add files via upload

---
+++ Task3/process.php
@@ -0,0 +1,35 @@
+<?php
+	     $formdata = array(
+	      'FirstName'=> $_POST["FirstName"],
+	      'LastName'=> $_POST["LastName"],
+          'Age'=> $_POST["Age"],
+          'Email'=>$_POST["Email"],
+	      'Password'=> $_POST["Password"]
+	   );
+       $existingdata = file_get_contents('data.json');
+       $tempJSONdata = json_decode($existingdata);
+       $tempJSONdata[] =$formdata;
+       //Convert updated array to JSON
+	   $jsondata = json_encode($tempJSONdata, JSON_PRETTY_PRINT);
+	   
+	   //write json data into data.json file
+	   if(file_put_contents("data.json", $jsondata)) {
+	        echo "Data successfully saved <br>";
+	    }
+	   else 
+	        echo "no data saved";
+
+     $data = file_get_contents("data.json");
+	 $mydata = json_decode($data);
+
+    
+	 echo "my value: ".$mydata[1]->LastName;
+/*	 foreach($mydata as $myobject)
+	 {
+	 foreach($myobject as $key=>$value)
+	{
+		echo "your ".$key." is ".$value."<br>";
+	} 
+	}
+	*/
+?>
